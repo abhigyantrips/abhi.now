7020cd1bfd95e9781c3a0fb1eae21ee2991807ba
Abhigyan Trips <contact@abhi.now>
2026-02-13 15:21:34 +0530

    feat(ui): add icons above labels

--- src/consts.ts
+++ src/consts.ts
@@ -1,21 +1,21 @@
 import type { Route, Site, Social } from '@/types';
 
 export const SITE: Site = {
-	TITLE: 'abhi.now',
-	DESCRIPTION: 'personal website of abhi',
+	TITLE: 'abhigyan trips.',
+	DESCRIPTION: 'working towards a more distraction-free internet.',
 	AUTHOR: 'Abhigyan Trips',
 	URL: 'https://abhi.now',
 };
 
 export const ROUTES: Route[] = [
-	{ NAME: 'whoami', PATH: '/whoami', ICON: '/icons/whoami.svg', COLOR: 'cyan' },
-	{ NAME: 'blog', PATH: '/blog', ICON: '/icons/blog.svg', COLOR: 'blue' },
-	{ NAME: 'snippets', PATH: '/snippets', ICON: '/icons/snippets.svg', COLOR: 'green' },
-	{ NAME: 'projects', PATH: '/projects', ICON: '/icons/projects.svg', COLOR: 'purple' },
-	{ NAME: 'anti-vision', PATH: '/anti-vision', ICON: '/icons/anti-vision.svg', COLOR: 'red' },
-	{ NAME: 'photos', PATH: '/photos', ICON: '/icons/photos.svg', COLOR: 'orange' },
-	{ NAME: 'media', PATH: '/media', ICON: '/icons/media.svg', COLOR: 'yellow' },
-	{ NAME: 'now', PATH: '/now', ICON: '/icons/now.svg', COLOR: 'pink' },
+	{ NAME: 'whoami', PATH: '/whoami', ICON: '/icons/question.svg', COLOR: 'grey' },
+	{ NAME: 'blog', PATH: '/blog', ICON: '/icons/note.svg', COLOR: 'blue' },
+	{ NAME: 'snippets', PATH: '/snippets', ICON: '/icons/photo.svg', COLOR: 'green' },
+	{ NAME: 'projects', PATH: '/projects', ICON: '/icons/automation.svg', COLOR: 'darkblue' },
+	{ NAME: 'anti-vision', PATH: '/anti-vision', ICON: '/icons/hide.svg', COLOR: 'red' },
+	{ NAME: 'people', PATH: '/people', ICON: '/icons/crown.svg', COLOR: 'orange' },
+	{ NAME: 'media', PATH: '/media', ICON: '/icons/movie-clapper.svg', COLOR: 'purple' },
+	{ NAME: 'now', PATH: '/now', ICON: '/icons/point-down.svg', COLOR: 'pink' },
 ];
 
 export const SOCIALS: Social[] = [
