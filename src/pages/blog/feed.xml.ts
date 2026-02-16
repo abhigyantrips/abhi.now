238d6ddb2574b9a2c10af9e0d7baae61fa1c65dc
Abhigyan Trips <contact@abhi.now>
2026-02-16 16:39:43 +0530

    feat(content): init RSS feed and add resume

---
+++ src/pages/blog/feed.xml.ts
@@ -0,0 +1,20 @@
+import { SITE } from '@/consts';
+import rss from '@astrojs/rss';
+import type { AstroSharedContext } from 'astro';
+import { getCollection } from 'astro:content';
+
+export async function GET(context: AstroSharedContext) {
+    const blog = await getCollection('blog');
+
+    return rss({
+        title: "abhigyan trips's blog.",
+        description: 'stories, ideas and random musings from a kid who likes chai.',
+        site: context.site || SITE.URL,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.date,
+            link: `/blog/${post.id}/`
+        }))
+    })
+}
