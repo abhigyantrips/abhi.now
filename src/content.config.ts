bd920c5709188863d1e2e1270063b729a77df931
Abhigyan Trips <contact@abhigyantrips.dev>
2026-02-20 08:58:55 +0530

    chore(content): init custom glob parser

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
+import { globWithParser } from "./lib/parsers";
 
 const blog = defineCollection({
 	loader: glob({
@@ -23,6 +24,41 @@ const tags = defineCollection({
 		name: z.string(),
 		description: z.string().optional(),
 		color: z.string(),
+	}),
+});
+
+const projects = defineCollection({});
+
+const snippets = defineCollection({
+	loader: globWithParser({
+		base: "./src/content/snippets",
+		pattern: "**/*.md",
+		parser: async (entry) => {
+			const { id, data }: { id: string; data: { title?: string; date?: string } } = entry;
+
+			data.title = id;
+			data.date = id.match(/(\d{4}-\d{2}-\d{2})/)?.[0] ?? new Date().toISOString();
+
+			return entry;
+		},
+	}),
+	schema: z.object({
+		title: z.string().default(""),
+		description: z.string().default("a sticky note by abhigyan trips."),
+		date: z.coerce.date().optional(),
+		tags: z.array(reference("tags")).optional(),
+	}),
+});
+
+const befores = defineCollection({
+	loader: glob({
+		base: "./src/content/befores",
+		pattern: "**/*.md",
+	}),
+	schema: z.object({
+		title: z.string().default(Math.floor(new Date().getTime() / 1000).toString()),
+		description: z.string(),
+		date: z.coerce.date(),
 	}),
 });
 
