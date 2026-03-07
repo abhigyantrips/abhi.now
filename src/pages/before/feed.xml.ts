10b4228c2ade224a4f03c35cd942da370af73a58
Abhigyan Trips <contact@abhigyantrips.dev>
2026-03-07 10:06:49 +0530

    chore(rss): add all feeds to base head

--- src/pages/before/feed.xml.ts
+++ src/pages/before/feed.xml.ts
@@ -7,7 +7,7 @@ export async function GET(context: AstroSharedContext) {
 	const before = await getCollection("before");
 
 	return rss({
-		title: "abhigyan trips's before.",
+		title: "abhigyan trips's 'now' archives'.",
 		description: "tiny tiny documentation from a kid who likes chai.",
 		site: context.site || SITE.URL,
 		items: before.map((post) => ({
