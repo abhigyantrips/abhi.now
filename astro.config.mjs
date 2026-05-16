0b7f86dc101fdaa1311027d5e878afd2f26b49e8
Abhigyan Trips <contact@abhigyantrips.dev>
2026-05-17 01:16:26 +0530

    feat(content): add images w/ captions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,31 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import rehypeFigure from "@microflash/rehype-figure";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: import.meta.env.DEV ? "http://localhost:4321" : "https://abhi.now",
-    output: "static",
+	site: import.meta.env.DEV ? "http://localhost:4321" : "https://abhi.now",
+	output: "static",
 
-    adapter: cloudflare({
-        imageService: "cloudflare",
-    }),
+	adapter: cloudflare({
+		imageService: "cloudflare",
+	}),
 
-    integrations: [sitemap(), mdx()],
+	integrations: [sitemap(), mdx()],
 
-    vite: {
-        plugins: [tailwindcss()],
-        build: {
-            rollupOptions: {
-                external: ["node:crypto", "node:fs/promises", "node:path", "node:url"],
-            },
-        },
-    },
+	markdown: {
+		rehypePlugins: [rehypeFigure],
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
+		build: {
+			rollupOptions: {
+				external: ["node:crypto", "node:fs/promises", "node:path", "node:url"],
+			},
+		},
+	},
 });
